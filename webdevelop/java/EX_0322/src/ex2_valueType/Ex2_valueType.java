321de72d472f1de7669bd3ba84b654c92c8166bd
정상필의GRAM\mod45 <mod459@naver.com>
2024-03-23 12:28:32 +0900

    0320 JAVA - push안한것들 추가

--- webdevelop/java/EX_0322/src/ex2_valueType/Ex2_valueType.java
+++ webdevelop/java/EX_0322/src/ex2_valueType/Ex2_valueType.java
@@ -4,7 +4,7 @@ public class Ex2_valueType {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		byte bNum = 123;
+		byte bNum = -128;
 		int num = 1234;
 		short shortNum = 12;
 		long lNum = 123;
@@ -15,6 +15,7 @@ public class Ex2_valueType {
 
 		
 		String Str = "가나다라";
+
 	}
 
 }
