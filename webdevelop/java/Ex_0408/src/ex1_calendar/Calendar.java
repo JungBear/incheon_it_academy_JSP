55d77763d1ccb862722bef3d89586431e18e9941
정상필의GRAM\mod45 <mod459@naver.com>
2024-04-08 15:26:10 +0900

    0408 - project명을 날짜로 변경

---
+++ webdevelop/java/Ex_0408/src/ex1_calendar/Calendar.java
@@ -0,0 +1,15 @@
+package ex1_calendar;
+
+public class Calendar {
+	
+	String color;
+	int month;
+	
+	
+	public Calendar(String color, int month) {
+		this.color = color;
+		this.month = month;
+	}
+
+	
+}
