31976a5986744cd5a0feb4a129d15d2538760e03
정상필의GRAM\mod45 <mod459@naver.com>
2024-04-14 12:38:47 +0900

    0414 front - React에서 데이터 export, import

--- front/react/react_shop/src/App.js
+++ front/react/react_shop/src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Nav, Navbar, Button} from 'react-bootstrap';
 
+import data from './data';
+import {num1, num2, num3} from './data';
+
 function App() {
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">{data}</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">{num1}</Nav.Link>
+            <Nav.Link href="#features">{num2}</Nav.Link>
+            <Nav.Link href="#pricing">{num3}</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
       <br/>
-      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="primary">primary</Button>{' '}
     </div>
   );
 }
