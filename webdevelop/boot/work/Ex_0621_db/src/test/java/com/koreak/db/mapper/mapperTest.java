0708810493525e0bfdc4911fc4dd132d59d8fbe7
정상필의GRAM\mod45 <mod459@naver.com>
2024-07-01 16:37:27 +0900

    0701 - config.xml 수정

--- webdevelop/boot/work/Ex_0621_db/src/test/java/com/koreak/db/mapper/mapperTest.java
+++ webdevelop/boot/work/Ex_0621_db/src/test/java/com/koreak/db/mapper/mapperTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.koreak.db.mapper.TimeMapper;
-
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
